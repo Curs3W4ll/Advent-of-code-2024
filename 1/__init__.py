ce1697d33224c5ce5a7aa34b93dad86a336a28b5
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-12-01 19:09:20 +0100

    feat(1): Add day 1 part 2

--- 1/__init__.py
+++ 1/__init__.py
@@ -3,6 +3,7 @@ from typing import List
 def run(input: List[str]):
     l1,l2 = [int(i.split()[0]) for i in input],[int(i.split()[1]) for i in input]
     total = 0
+    similarityScore = 0
 
     l1.sort()
     l2.sort()
@@ -10,3 +11,8 @@ def run(input: List[str]):
     for i in range(len(l1)):
         total += abs(l1[i] - l2[i])
     print(f"Total distance is: {total}")
+
+    for i in range(len(l1)):
+        if l1[i] in l2:
+            similarityScore += (l1[i] * l2.count(l1[i]))
+    print(f"Similarity score is: {similarityScore}")
