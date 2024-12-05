57cfc57c0986f293d6e8a00e5f341ac140027665
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-12-05 14:43:04 +0100

    feat(4): Add day 4 part 2

--- 4/__init__.py
+++ 4/__init__.py
@@ -1,27 +1,24 @@
-searchingWord = "XMAS"
-searchingWord = searchingWord.upper()
+def isMatchingSearchWord(s: str, searchWord: str):
+    return (s == searchWord or s == searchWord[::-1])
 
-def isMatchingSearchWord(s: str):
-    return (s == searchingWord or s == searchingWord[::-1])
-
-def run(input: list[str]):
+def part1(input: list[str]):
+    searchingWord = "XMAS"
+    searchingWord = searchingWord.upper()
     searchingWordLen = len(searchingWord)
     wordCount = 0
 
-    for i in range(len(input)):
-        input[i] = input[i].upper()
     for i in range(len(input)):
         for y in range(len(input[i])):
             if input[i][y] != "X":
                 continue
             # Right
             if y <= (len(input[i]) - searchingWordLen):
-                if isMatchingSearchWord(input[i][y:y + searchingWordLen]):
+                if isMatchingSearchWord(input[i][y:y + searchingWordLen], searchingWord):
                     wordCount += 1
 
             # Left
             if y >= (searchingWordLen - 1):
-                if isMatchingSearchWord(input[i][y - searchingWordLen + 1:y + 1]):
+                if isMatchingSearchWord(input[i][y - searchingWordLen + 1:y + 1], searchingWord):
                     wordCount += 1
 
             # Top
@@ -29,7 +26,7 @@ def run(input: list[str]):
                 w = ""
                 for j in range(i, i - searchingWordLen, -1):
                     w += input[j][y]
-                if isMatchingSearchWord(w):
+                if isMatchingSearchWord(w, searchingWord):
                     wordCount += 1
 
                 # Top Right
@@ -39,7 +36,7 @@ def run(input: list[str]):
                     for j in range(i, i - searchingWordLen, -1):
                         w += input[j][k]
                         k += 1
-                    if isMatchingSearchWord(w):
+                    if isMatchingSearchWord(w, searchingWord):
                         wordCount += 1
 
                 # Top Left
@@ -49,7 +46,7 @@ def run(input: list[str]):
                     for j in range(i, i - searchingWordLen, -1):
                         w += input[j][k]
                         k -= 1
-                    if isMatchingSearchWord(w):
+                    if isMatchingSearchWord(w, searchingWord):
                         wordCount += 1
 
             # Bottom
@@ -57,7 +54,7 @@ def run(input: list[str]):
                 w = ""
                 for j in range(i, i + searchingWordLen):
                     w += input[j][y]
-                if isMatchingSearchWord(w):
+                if isMatchingSearchWord(w, searchingWord):
                     wordCount += 1
 
                 # Bottom Right
@@ -67,7 +64,7 @@ def run(input: list[str]):
                     for j in range(i, i + searchingWordLen):
                         w += input[j][k]
                         k += 1
-                    if isMatchingSearchWord(w):
+                    if isMatchingSearchWord(w, searchingWord):
                         wordCount += 1
 
                 # Bottom Left
@@ -77,7 +74,42 @@ def run(input: list[str]):
                     for j in range(i, i + searchingWordLen):
                         w += input[j][k]
                         k -= 1
-                    if isMatchingSearchWord(w):
+                    if isMatchingSearchWord(w, searchingWord):
                         wordCount += 1
 
-    print(f"The input contains {wordCount} words!")
+    return wordCount
+
+def part2(input: list[str]):
+    searchingWord = "MAS"
+    searchingWord = searchingWord.upper()
+    searchingWordLen = len(searchingWord)
+    count = 0
+
+    for i in range(len(input) - searchingWordLen + 1):
+        for y in range(len(input[i]) - searchingWordLen + 1):
+            k = y
+
+            # Top left to Bottom right
+            w = ""
+            for j in range(i, i + searchingWordLen):
+                w += input[j][k]
+                k += 1
+            if isMatchingSearchWord(w, searchingWord):
+                # Top right to Bottom left
+                w = ""
+                k -= 1
+                for j in range(i, i + searchingWordLen):
+                    w += input[j][k]
+                    k -= 1
+                if isMatchingSearchWord(w, searchingWord):
+                    count += 1
+
+    return count
+
+def run(input: list[str]):
+    for i in range(len(input)):
+        input[i] = input[i].upper()
+
+    print(f"The input contains {part1(input)} words!")
+
+    print(f"The input contains {part2(input)} X-MAS!")
