08b64d655c218d10f9729a3e6d3b4776bb90b160
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-12-07 11:48:15 +0100

    feat(7): Add day 7 part 2

--- 7/__init__.py
+++ 7/__init__.py
@@ -1,10 +1,22 @@
 def add(x: int, y: int) -> int: return x + y
 def mul(x: int, y: int) -> int: return x * y
-operations = [add, mul]
+def concat(x: int, y: int) -> int: return int(f"{x}{y}")
+operations = [add, mul, concat]
+
+def toBase(number: int, base: int):
+    if number == 0:
+        return "0"
+
+    digits = []
+
+    while number > 0:
+        digits.append(str(number % base))
+        number //= base
+    return "".join(reversed(digits))
 
 def applyOperations(numbers: list[int], number: int):
     value = numbers[0]
-    ops = [int(x) for x in bin(number)[2:].zfill(len(numbers))]
+    ops = [int(x) for x in toBase(number, len(operations)).zfill(len(numbers))]
 
     for i in range(1, len(numbers)):
         value = operations[ops[i - 1]](value, numbers[i])
