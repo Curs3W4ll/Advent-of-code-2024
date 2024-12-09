46a1703aa72b3dcd2731702e1ecf2db432653123
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-12-09 12:38:13 +0100

    feat(9): Add day 9 part 2

--- 9/__init__.py
+++ 9/__init__.py
@@ -1,3 +1,5 @@
+import copy
+
 spaceId = -1
 
 def dumpCases(cases: list[int]):
@@ -8,10 +10,17 @@ def dumpCases(cases: list[int]):
             print(case, end="")
     print("")
 
-def getFirstSpaceIndex(cases: list[int]) -> int:
+def isFullOfSpaces(cases: list[int]) -> bool:
+    for case in cases:
+        if case != spaceId:
+            return False
+    return True
+
+def getFirstSpaceIndex(cases: list[int], length: int = 1) -> int:
     for i in range(len(cases)):
-        if cases[i] == spaceId:
+        if isFullOfSpaces(cases[i:i + length]):
             return i
+
     return len(cases)
 
 def getChecksum(cases: list[int]) -> int:
@@ -19,7 +28,7 @@ def getChecksum(cases: list[int]) -> int:
 
     for i in range(len(cases)):
         if cases[i] == spaceId:
-            break
+            continue
         total += cases[i] * i
 
     return total
@@ -37,6 +46,26 @@ def fragment(cases: list[int]) -> int:
 
     return getChecksum(cases)
 
+def fragmentKeepFiles(cases: list[int]) -> int:
+    i = len(cases) - 1
+    while i >= 0:
+        currentId = cases[i]
+        if currentId == spaceId:
+            i -= 1
+            continue
+        length = 1
+        while i - length > 0 and cases[i - length] == currentId:
+            length += 1
+        spaceIndex = getFirstSpaceIndex(cases, length)
+        # Fit possible
+        if not spaceIndex > i:
+            for l in range(length):
+                cases[spaceIndex + l] = currentId
+                cases[i - l] = spaceId
+        i -= length
+
+    return getChecksum(cases)
+
 def run(input: list[str]):
     cases: list[int] = []
 
@@ -48,5 +77,9 @@ def run(input: list[str]):
         else:
             cases += [spaceId for _ in range(int(input[0][i]))]
 
+    dumpCases(cases)
+    casesCopy = copy.copy(cases)
+    print(f"Filesystem checksum is {fragment(casesCopy)}")
+    # dumpCases(casesCopy)
+    print(f"Filesystem checksum is {fragmentKeepFiles(cases)} when keeping files continuity")
     # dumpCases(cases)
-    print(f"Filesystem checksum is {fragment(cases)}")
